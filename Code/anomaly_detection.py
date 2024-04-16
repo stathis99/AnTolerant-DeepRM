bf2f6889abab5bc40cbf2b9039815af9f82f1355
stathis99 <stathistrigas99@gmail.com>
2024-04-16 13:11:56 +0200

    generate anomalous jobs with lognorm distribution

--- Code/anomaly_detection.py
+++ Code/anomaly_detection.py
@@ -44,6 +44,46 @@ def bi_model_dist(num_res, max_nw_size, job_len):
     return nw_len, nw_size    
 
 
+def log_norm_dist(num_res, max_nw_size, job_len):
+    num_res = num_res
+    max_nw_size = max_nw_size
+    job_len = job_len
+
+    job_small_chance = 0.8
+
+    job_len_big_lower = job_len * 2 / 3
+    job_len_big_upper = job_len
+
+    job_len_small_lower = 1
+    job_len_small_upper = job_len / 5
+
+    dominant_res_lower = max_nw_size / 2
+    dominant_res_upper = max_nw_size
+
+    other_res_lower = 1
+    other_res_upper = max_nw_size / 5
+
+    mu_lenght = 2  
+    sigma_lenght = 0.5
+    mu_res = 2.5
+    sigma_res = 0.4 
+      
+    nw_len = np.round(np.random.lognormal(mu_lenght, sigma_lenght, 1)).astype(int)
+
+
+    nw_size = np.zeros(num_res)
+
+    # -- job resource request --
+    dominant_res = np.random.randint(0, num_res)
+    for i in range(num_res):
+        if i == dominant_res:
+            nw_size[i] = np.round(np.random.lognormal(mu_res, sigma_res, 1)).astype(int)
+
+        else:
+            nw_size[i] = np.round(np.random.lognormal(mu_res, sigma_res, 1)).astype(int)
+
+    return nw_len, nw_size    
+
     
 def generate_sequence_work(simu_len):
 
@@ -52,11 +92,21 @@ def generate_sequence_work(simu_len):
 
     for i in range(simu_len):
 
-        if np.random.rand() < 0.7:  # a new job comes
+        if np.random.rand() < 2:  # a new job comes
 
-            nw_len_seq[i], nw_size_seq[i, :] = bi_model_dist(2,10,15)
+            nw_len_seq[i], nw_size_seq[i, :] = log_norm_dist(2,10,15)
 
     return nw_len_seq, nw_size_seq
+
+def save_jobs_csv(nw_len_seq, nw_size_seq, filename="jobs.csv"):
+
+    # Combine job length and size information into a single array for easier saving
+    data = np.column_stack((nw_len_seq, nw_size_seq))
+
+    # Save the data to a CSV file with headers
+    np.savetxt(filename, data, delimiter=",", header="JobLength,Size1,Size2", fmt="%d")  # Assuming size has 2 dimensions
+
+
 
 
 def launch(pa):
@@ -66,4 +116,6 @@ def launch(pa):
     nw_len_seqs, nw_size_seqs = generate_sequence_work(pa.simu_len * pa.num_ex)
     print(nw_len_seqs)
     print(nw_size_seqs)
+
+    save_jobs_csv(nw_len_seqs, nw_size_seqs)
     print("done !")
